08e9f8a09fc0bb9e9686d6d06866dd52ec06b72c
demiourglass <45144536+demiourglass@users.noreply.github.com>
2019-04-28 04:59:25 -0400

    Create installPyCentOS.sh

---
+++ installPyCentOS.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+sudo yum update
+sudo yum install -y python36u python36u-libs python36u-devel python36u-pip
+#python3.6 -V
